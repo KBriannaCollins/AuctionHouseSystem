cf0a4bf448b429de5366a1156273595211618960
Kearsten <k.brianna.collins@gmail.com>
2020-07-03 13:13:19 -0500

    adding unit testing

---
+++ auctionhouse/unittest/test_model_auctions.py
@@ -0,0 +1,7 @@
+'''Unit testing for classes Product, Auction, and Bid'''
+import unittest
+
+# from auctionhouse.models.auctions import User, Bidder, Employee
+
+if __name__ == '__main__':
+    unittest.main()
