9ef885a3a5684e5763674d95c00de41eece41932
neeten97 <nathanieleeten@gmail.com>
2020-07-08 14:27:27 -0600

    Sceduler started
    Created a scheduler

--- auctionhouse/app.py
+++ auctionhouse/app.py
@@ -2,6 +2,9 @@
 
 from flask import Flask, request, make_response, jsonify, render_template
 from flask_cors import CORS
+import time
+import atexit
+from apscheduler.schedulers.background import BackgroundScheduler
 from auctionhouse.router.users import users
 from auctionhouse.router.auctions import auctions
 from auctionhouse.router.products import products
@@ -9,6 +12,15 @@ import werkzeug
 
 #Initialize Flask
 app = Flask(__name__)
+
+#initialize scheduler
+scheduler = BackgroundScheduler()
+
+def print_date_time():
+    print(time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
+
+scheduler.add_job(func=print_date_time, trigger="interval", seconds=3)
+scheduler.start()
 
 #initialize CORS
 CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
@@ -19,6 +31,8 @@ CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 app.register_blueprint(users)
 app.register_blueprint(auctions)
 app.register_blueprint(products)
+
+atexit.register(lambda: scheduler.shutdown())
 
 @app.route('/')
 def entry_page():
