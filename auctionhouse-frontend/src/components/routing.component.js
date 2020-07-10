8d28882e9463d6768f83d6eda8c568f556c310ad
Kearsten <k.brianna.collins@gmail.com>
2020-07-10 10:46:56 -0500

    more conditional routing changes

--- auctionhouse-frontend/src/components/routing.component.js
+++ auctionhouse-frontend/src/components/routing.component.js
@@ -18,13 +18,23 @@ function UserNav(props) {
     const UserLoggedIn = props.user
     if (UserLoggedIn && 'role' in UserLoggedIn) {
         if (UserLoggedIn.role.toUpper() === 'MANAGER'){
-            return <Nav className="mr-auto"><Link to='/auctionlist' className='nav_link'><h3>Auctions</h3></Link></Nav>
+            return <Nav className="mr-auto">
+                        <Link to='/userlist' className='nav_link'><h3>Users List</h3></Link>
+                        <Link to='/managelist' className='nav_link'><h3>Manage Auction</h3></Link>
+                        <Link to='/auctionlist' className='nav_link'><h3>Auctions</h3></Link>
+                    </Nav>
         } else if (UserLoggedIn.role.toUpper() === 'AUCTIONEER'){
-            return <Nav className="mr-auto"><Link to='/managelist' className='nav_link'><h3>Manage Auction</h3></Link></Nav>
+            return <Nav className="mr-auto">
+                        <Link to='/managelist' className='nav_link'><h3>Manage Auction</h3></Link>
+                    </Nav>
         } else //(UserLoggedIn.role.toUpper() === 'CURATOR'){
-            return <Nav className="mr-auto"><Link to='/auctionlist' className='nav_link'><h3>Auctions</h3></Link></Nav>     
-        }
-    else {
+            return <Nav className="mr-auto">
+                        <Link to='/auctionlist' className='nav_link'><h3>Auctions</h3></Link>
+                    </Nav>     
+    } else if (UserLoggedIn && 'history' in UserLoggedIn){
+        return
+
+    } else {
         return <Nav className="mr-auto"></Nav>
     }
 }
@@ -42,8 +52,6 @@ class Routing extends Component{
                 <Navbar>
                     <Navbar.Brand><Link to='/' className='nav_link'><h1>KTMN Auction House</h1></Link></Navbar.Brand>
                     <UserNav user={this.props.user}></UserNav>
-                    <Nav className="mr-auto"><Link to='/managelist' className='nav_link'><h3>Manage Auction</h3></Link></Nav>
-                    <Nav className="mr-auto"><Link to='/userlist' className='nav_link'><h3>Users List</h3></Link></Nav>
                     <Login></Login>
                 </Navbar>
 
