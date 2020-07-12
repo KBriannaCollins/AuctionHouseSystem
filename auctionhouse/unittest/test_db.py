26189fb9181f9d1a937029425b09e38007959eb0
neeten97 <nathanieleeten@gmail.com>
2020-07-12 13:23:37 -0600

    started unit tests for db

--- auctionhouse/unittest/test_db.py
+++ auctionhouse/unittest/test_db.py
@@ -1,4 +1,23 @@
+''' Database Test Cases '''
+
+# External Modules
+import unittest
+import unittest.mock as mock
+
 from auctionhouse.data.db import read_auctions_from_query
 
-# if __name__=="__main__":
-#     read_auctions_from_query({})
+
+from auctionhouse.data.db import read_all_users
+
+class TestClassDB(unittest.TestCase):
+    ''' The DB test suite '''
+    
+    @mock.patch(users.find) # Mock the users.find function
+    def test_read_all_users(self, mock_find):
+        mock_find.return_value = {'_id':'id'} # set the return value of the mock
+
+        self.assertTrue(mock_find.called)
+
+
+if __name__ == '__main__':
+    unittest.main()
